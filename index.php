9ffe563be529f16c345ae268105abbef67194c67
Nils Persson <nils@nedge.se>
2017-09-13 11:05:45 +0200

    Updated plugin header

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 /*
-  Plugin Name: AtF Spark by Nedge
-  Description: A simple plugin to enable inline charts powered by the Spark font by After the Flood
+  Plugin Name: Sparkline
+  Plugin URI: https://github.com/nilspersson/AtF-Spark
+  Description: A simple plugin to enable inline charts powered by the Spark font by After the Flood.
   Version: 0.1
   Author: Nils Persson
   Author URI: http://nilspersson.se/
