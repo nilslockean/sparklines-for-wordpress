96c91e10e6123781f669af00f1b561c58204b038
Nils Persson <nils@nedge.se>
2017-09-13 11:00:54 +0200

    Clean slate

--- index.php
+++ index.php
@@ -1,52 +1,12 @@
 <?php
-/**
-  * Plugin Name: AtF Spark by Nedge
-  * Plugin URI: http://nilspersson.se/
-  * Version: 0.1
-  * Author: Nils Persson
-  * Author URI: http://nilspersson.se/
-  * Description: A simple plugin to enable inline charts powered by the Spark font by After the Flood
-  */
+/*
+  Plugin Name: AtF Spark by Nedge
+  Description: A simple plugin to enable inline charts powered by the Spark font by After the Flood
+  Version: 0.1
+  Author: Nils Persson
+  Author URI: http://nilspersson.se/
+*/
 
-// defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
-
-class AtF_Spark_Class {
-  // Constructor. Called when the plugin is initialised.
-  function __construct() {
-    function setup_tinymce_plugin() {
-      // Check if the logged in WordPress User can edit Posts or Pages
-      // If not, don't register our TinyMCE plugin
-      if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
-        return;
-      }
-
-      // Check if the logged in WordPress User has the Visual Editor enabled
-      // If not, don't register our TinyMCE plugin
-      if ( get_user_option( 'rich_editing' ) !== 'true' ) {
-        return;
-      }
-
-      // Setup some filters
-      add_filter( 'mce_external_plugins', array( &$this, 'add_tinymce_plugin' ) );
-      add_filter( 'mce_buttons', array( &$this, 'add_tinymce_toolbar_button' ) );
-    }
-    if ( is_admin() ) {
-      add_action( 'init', array(  $this, 'setup_tinymce_plugin' ) );
-    }
-
-    function add_tinymce_plugin( $plugin_array ) {
-      $plugin_array['custom_link_class'] = plugin_dir_url( __FILE__ ) . 'tinymce-custom-link-class.js';
-      return $plugin_array;
-    }
-
-    function add_tinymce_toolbar_button( $buttons ) {
-      array_push( $buttons, '|', 'custom_link_class' );
-      return $buttons;
-    }
-  }
-
-}
-
-$atf_spark_class = new AtF_Spark_Class;
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 ?>
