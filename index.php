7a98f6fe56ce7de80eae641a7e0a998fb072d678
Nils Persson <nils@nedge.se>
2017-09-13 11:10:52 +0200

    Added [spark] shortcode

--- index.php
+++ index.php
@@ -10,4 +10,37 @@
 
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
+// The Shortcode
+function atf_spark_shortcode( $atts , $content = null ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'start' => '10',
+			'end' => '20',
+      'type' => 'bar-medium',
+		),
+		$atts,
+		'spark'
+	);
+
+  // Filter data
+  $noSpaces = str_replace(" ", "", $content);
+  $allValues = explode(",", $noSpaces);
+  $validValues = array_filter($allValues, function($x) {
+    return is_numeric($x);
+  });
+  $dataString = implode(",", $validValues);
+
+  // Return HTML
+	return '<span class="atf-spark--' . $atts['type'] . '">'
+    . $atts['start']
+    . '{'
+    . $dataString
+    . '}'
+    . $atts['end']
+    . '</span>';
+}
+add_shortcode( 'spark', 'atf_spark_shortcode' );
+
 ?>
